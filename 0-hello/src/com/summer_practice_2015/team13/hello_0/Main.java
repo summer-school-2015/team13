efaac733458d9bb0a4f6d912b03c39a75ffe6470
Dmitry Chepurovskiy <dm3ch@technical-craft.ru>
2015-06-02 10:37:33 +0300

    Task 0: hello-0

---
+++ 0-hello/src/com/summer_practice_2015/team13/hello_0/Main.java
@@ -0,0 +1,10 @@
+package com.summer_practice_2015.team13.hello_0;
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+}
---
+++ 0-hello/src/compile.bat
@@ -0,0 +1,2 @@
+javac com\summer_practice_2015\team13\hello_0\Main.java
+pause
