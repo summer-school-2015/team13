af6ba0b58cfb926783336ac41a7a4f6554c396b3
Dmitry Chepurovskiy <dm3ch@technical-craft.ru>
2015-06-04 11:03:03 +0300

    Task 5 Pro: Igor finaly changed try-catch construction

--- 3-sortex/src/com/summer_practice_2015/team13/sortex_3/Main.java
+++ 3-sortex/src/com/summer_practice_2015/team13/sortex_3/Main.java
@@ -2,69 +2,66 @@ package com.summer_practice_2015.team13.sortex_3;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
-
-    public static void main(String[] args) throws  FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         boolean flag = false;
         int n = 0;
-        try{
+        try {
             n = in.nextInt();
-        }
-        catch(InputMismatchException e){
+        } catch (InputMismatchException e) {
             out.println("Input error");
-            out.println();
-            out.flush();
-            flag = true;
+            out.close();
+            System.exit(0);
         }
         Figure figures[] = new Figure[n];
         TreeSet<Figure> f = new TreeSet<Figure>();
         ArrayList<Figure> f1 = new ArrayList<Figure>();
-        if (!flag) {
-            for (int i = 0; i < n; i++) {
-                String s = in.next();
-                try {
-                    if (s.charAt(0) == 'C') {
-                        int r = in.nextInt();
-                        figures[i] = new Circle(r);
-                    }
-                    if (s.charAt(0) == 'T') {
-                        int a = in.nextInt();
-                        int h = in.nextInt();
-                        figures[i] = new Triangle(a, h);
-                    }
-                    if (s.charAt(0) == 'R') {
-                        int a = in.nextInt();
-                        int b = in.nextInt();
-                        figures[i] = new Rectangle(a, b);
-                    }
-                    f.add(figures[i]);
-                    f1.add(figures[i]);
-                } catch (InputMismatchException e) {
-                    out.println("Input error");
-                    out.println();
-                    out.flush();
-                    flag = true;
-                    break;
+        for (int i = 0; i < n; i++) {
+            String s = in.next();
+            try {
+                if (s.charAt(0) == 'C') {
+                    int r = in.nextInt();
+                    figures[i] = new Circle(r);
                 }
+                if (s.charAt(0) == 'T') {
+                    int a = in.nextInt();
+                    int h = in.nextInt();
+                    figures[i] = new Triangle(a, h);
+                }
+                if (s.charAt(0) == 'R') {
+                    int a = in.nextInt();
+                    int b = in.nextInt();
+                    figures[i] = new Rectangle(a, b);
+                }
+                f.add(figures[i]);
+                f1.add(figures[i]);
+            } catch (InputMismatchException e) {
+                out.println("Input error");
+                out.close();
+                System.exit(0);
             }
         }
-        if (!flag) {
-            Arrays.sort(figures);
-            f1.sort(Comparator.<Figure>naturalOrder());
+
+        Arrays.sort(figures);
+        f1.sort(Comparator.<Figure>naturalOrder());
         /*
+        chose one variant
+
         for (int i = 0; i < n; i++) {
             figures[i].print_square();
-        }*/
-        /*
+        }
+
         while (f.size() > 0){
             f.pollFirst().print_square();
-        }*/
-            for (Figure q : f1) {
-                q.print_square();
-            }
+        }
+
+        */
+        for (Figure q : f1) {
+            q.print_square();
         }
         out.close();
     }
