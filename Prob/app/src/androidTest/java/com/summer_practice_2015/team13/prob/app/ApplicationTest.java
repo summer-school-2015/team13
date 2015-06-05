b78690a4c9a4f5ff270c747704d29a683606e447
Dmitry Chepurovskiy <dm3ch@technical-craft.ru>
2015-06-05 10:36:16 +0300

    Task 6: Not working

---
+++ Prob/app/src/androidTest/java/com/summer_practice_2015/team13/prob/app/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.summer_practice_2015.team13.prob.app;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
